80edf3c4afecc6bc095252e900378f0909dccd97
Scott  Keplinger <skeplin17@gmail.com>
2020-05-14 01:25:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor
+  attr_reader :add_student, :school_name, :roster
+
+  def initialize (school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+
+
+  def add_student(student, grade_level)
+    @student = student
+    @grade_level = grade_level
+
+      if @roster.include?(grade_level)
+           @roster[grade_level] << student
+      else @roster[grade_level] = []
+           @roster[grade_level] << student
+      end
+   end
+
+  def grade(grade)
+     @roster[grade]
+  end
+
+  def sort
+    @roster.collect do |grade, student|
+
+  end
+end
